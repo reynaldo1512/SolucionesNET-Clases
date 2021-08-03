fb55f8a11afa83924801da90bf9452229dd7f005
REYNALDO MAURO VASQUEZ CONTRERAS <reynaldovc1512@gmail.com>
2021-08-03 13:58:45 -0500

    avance

---
+++ LABORATORIORVC/LABORATORIO/Lab/Controllers/PacienteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAO;
+
+namespace Lab.Controllers
+{
+    public class PacienteController : Controller
+    {
+        private Conn_DB conn = new Conn_DB();
+        // GET: Paciente
+        public ActionResult Index()
+        {
+            return View(conn.tbPaciente.ToList());
+        }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            ViewBag.ListaSexo = conn.tbSexo.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(tbPaciente paci)
+        {
+            conn.tbPaciente.Add(paci);
+            conn.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LABORATORIORVC/LABORATORIO/Lab/Controllers/TipoExamenController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAO;
+
+namespace Lab.Controllers
+{
+    public class TipoExamenController : Controller
+    {
+        private Conn_DB conn = new Conn_DB();
+        // GET: TipoExamen
+        public ActionResult Index()
+        {
+            return View(conn.tbExamen.ToList());
+        }
+        [HttpGet]
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(tbExamen exam)
+        {
+            conn.tbExamen.Add(exam);
+            conn.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Editar(int id)
+        {
+            tbExamen exam = new tbExamen();
+            exam = conn.tbExamen.Find(id);
+            return View(exam);
+        }
+        [HttpPost]
+        public ActionResult Editar(tbExamen examnew)
+        {
+            tbExamen exam = new tbExamen();
+            exam = conn.tbExamen.Find(examnew.idExamen);
+            exam.nombreExamen = examnew.nombreExamen;
+            exam.precioUnidad = examnew.precioUnidad;
+            exam.estado = examnew.estado;
+            conn.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Eliminar(int id)
+        {
+            tbExamen exam = new tbExamen();
+            exam = conn.tbExamen.Find(id);
+            exam.estado = false;
+            conn.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+
+    }
+}
