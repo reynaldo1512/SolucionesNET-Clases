258590331528e054410075d53a9988a04520bb6e
REYNALDO MAURO VASQUEZ CONTRERAS <reynaldovc1512@gmail.com>
2021-08-04 14:38:15 -0500

    Avance

--- LABORATORIORVC/LABORATORIO/Lab/Controllers/PacienteController.cs
+++ LABORATORIORVC/LABORATORIO/Lab/Controllers/PacienteController.cs
@@ -28,5 +28,34 @@ namespace Lab.Controllers
             conn.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Editar(int id)
+
+        {
+            
+            tbPaciente paciente = new tbPaciente();
+            paciente = conn.tbPaciente.Find(id);
+            ViewBag.ListaSexo = conn.tbSexo.ToList();
+            return View(paciente);
+
+            
+        }
+        [HttpPost]
+        public ActionResult Editar(tbPaciente pacientenew)
+        {
+            tbPaciente paciente = new tbPaciente();
+            paciente = conn.tbPaciente.Find(pacientenew.idPaciente);
+            paciente.dni = pacientenew.dni;
+            paciente.nombrePaciente = pacientenew.nombrePaciente;
+            paciente.apellidoPaciente = pacientenew.apellidoPaciente;
+            paciente.direccion = pacientenew.direccion;
+            paciente.idSexo = pacientenew.idSexo;
+            paciente.telefono = pacientenew.telefono;
+            paciente.fechaNacimiento = pacientenew.fechaNacimiento;
+            conn.SaveChanges();
+            return RedirectToAction("Index");
+
+            
+
+        }
     }
 }
